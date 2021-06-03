7302ffff3f65daa64e3496c42944307e3afb6e4b
aaryapatel <aarya.patel@mail.utoronto.ca>
2021-06-03 10:32:55 -0400

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,32 +3,31 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    siteUrl: `https://www.example.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `docs`,
+        path: `${__dirname}/src/docs`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-transformer-remark",
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        plugins: [
+          {
+            resolve: "gatsby-remark-autolink-headers",
+            options: {
+              offsetY: "100",
+              isIconAfterHeader: true,
+            },
+          },
+        ],
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,72 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require(`path`)
+const { slash } = require(`gatsby-core-utils`)
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const blogTemplate = path.resolve("./src/templates/BlogTemplate.js")
+
+  return graphql(
+    `
+      {
+        allMarkdownRemark(
+          limit: 1000
+          filter: { frontmatter: { draft: { ne: true } } }
+        ) {
+          edges {
+            node {
+              frontmatter {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `
+  ).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    // Create blog posts pages.
+    result.data.allMarkdownRemark.edges.forEach(edge => {
+      createPage({
+        path: edge.node.frontmatter.slug, // required
+        component: slash(blogTemplate),
+        context: {
+          slug: edge.node.frontmatter.slug,
+        },
+      })
+    })
+  })
+}
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const fileNode = getNode(node.parent)
+    let nodeSlug
+    nodeSlug = ensureSlashes(
+      path.basename(fileNode.relativePath, path.extname(fileNode.relativePath))
+    )
+    if (nodeSlug) {
+      createNodeField({ node, name: `slug`, value: nodeSlug })
+    }
+  }
+}
+
+function ensureSlashes(slug) {
+  if (slug.charAt(0) !== `/`) {
+    slug = `/` + slug
+  }
+
+  if (slug.charAt(slug.length - 1) !== `/`) {
+    slug = slug + `/`
+  }
+
+  return slug
+}
