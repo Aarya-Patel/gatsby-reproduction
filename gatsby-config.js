de64f8f676eab085deb89552f82385894d6cb948
aaryapatel <aarya.patel@mail.utoronto.ca>
2021-06-03 10:37:06 -0400

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
           {
             resolve: "gatsby-remark-autolink-headers",
             options: {
-              offsetY: "100",
+              offsetY: 100,
               isIconAfterHeader: true,
             },
           },
